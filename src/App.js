bb1f9d46cd025aed4c105d72a6d95f173210fb7b
Mayur Saroj <mayursarojofficial@gmail.com>
2022-08-05 00:12:10 +0530

    :lipstick: Design Hero Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="font-mono bg-slate-100 dark:bg-slate-900 min-h-screen">
+    <div className="font-mono bg-white dark:bg-neutral-900 min-h-screen">
       <div className="max-w-5xl mx-auto">
         <Navbar />
+        <HeroSection />
       </div>
     </div>
   );
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Envelope from "./svg/Envelope";
+import GithubLogo from "./svg/GithubLogo";
+import LinkedinLogo from "./svg/LinkedinLogo";
+
+const HeroSection = () => {
+  return (
+    <header className="p-4 dark:text-slate-100">
+      <h1 className="text-2xl font-bold">
+        Hello<span className="text-3xl">👋</span>,<br /> i'm Mayur Saroj
+      </h1>
+      <p className="mt-2">
+        A developer who is passionate about building applications, adapts very
+        quickly to emerging frameworks, languages and loves exploring new
+        technologies to integrate them together for solving real world problems
+      </p>
+      <div className="mt-4 flex flex-col gap-1">
+        <a className="flex gap-1" href="mailto:mayursdev@gmail.com">
+          <Envelope className="dark:fill-slate-100" />
+          <span className="underline">mayursdev@gmail.com</span>
+        </a>
+        <a
+          className="flex gap-1"
+          href="https://github.com/mayursdev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GithubLogo className="dark:fill-slate-100" />
+          <span className="underline">mayursdev</span>
+        </a>
+        <a
+          className="flex gap-1"
+          href="https://www.linkedin.com/in/mayursdev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedinLogo className="dark:fill-slate-100" />
+          <span className="underline">mayursdev</span>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default HeroSection;
